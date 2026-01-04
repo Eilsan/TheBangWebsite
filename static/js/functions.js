c69d75603fab3ade7336ff35d5eafcd83a72ecbb
52kyrjak <52kyrjak@stud.akademiet.no>
2026-01-04 12:36:11 +0100

    fixa jævla mye piss

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,11 +1,11 @@
 export async function getSong(album) {
-    const response = await fetch('./static/json/main.json')
+    const response = await fetch('/static/json/main.json')
     const data = await response.json()
     return data[album]
 }
 
 export async function dumpJSON() {
-    const response = await fetch('..//json/main.json')
+    const response = await fetch('/static/json/main.json')
     const data = await response.json()
     return data
 }
